bd2d035ffae9d0f34684326ccaa3cbaa89daafa7
Roman Averkin <romaaverkin@gmail.com>
2018-10-14 01:32:00 +0700

    Изменил DefaultController на GenusController

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        return new Response("Род: " . $genusName);
+    }
+}
