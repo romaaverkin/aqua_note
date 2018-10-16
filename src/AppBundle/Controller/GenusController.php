cdef4d867bc00acf8a8cf5720cc0a08cd562b6d1
Roman <romaaverkin@gmail.com>
2018-10-16 16:41:57 +0700

    twig

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,16 +3,26 @@
 namespace AppBundle\Controller;
 
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
-class GenusController
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)
     {
-        return new Response("Род: " . $genusName);
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+
+        return $this->render('genus/show.html.twig',
+            [
+                'name' => $genusName,
+                'notes' => $notes
+            ]);
     }
 }
