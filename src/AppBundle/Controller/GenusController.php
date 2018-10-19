3f1ce65c16440e4e508c152f4572ce90bb77bb01
Roman <romaaverkin@gmail.com>
2018-10-19 13:44:14 +0700

    Использование Services

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -13,9 +13,15 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
+        $funFact = "Octopuses can change the color of their body in just *three-tenths* of a second!";
+
+        $funFact = $this->get('markdown.parser')
+            ->transform($funFact);
+
         return $this->render('genus/show.html.twig',
             [
                 'name' => $genusName,
+                'funFact' => $funFact,
             ]);
     }
 
